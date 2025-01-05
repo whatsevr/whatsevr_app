c7e655cbb55886469d85885fd9266db74f21fed6
Nayeem Akhtar <nayeem.akhtar@fitpass.co.in>
2025-01-06 00:17:55 +0530

    feat: add timeago package and integrate bookmarks view into HomePage

--- lib/src/features/home/views/page.dart
+++ lib/src/features/home/views/page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:intl/intl.dart';
+import 'package:timeago/timeago.dart' as timeago;
 import 'package:whatsevr_app/config/themes/theme.dart';
 
 import 'package:whatsevr_app/config/widgets/pad_horizontal.dart';
@@ -9,6 +11,7 @@ import 'package:whatsevr_app/src/features/home/views/widgets/communities.dart';
 import 'package:whatsevr_app/src/features/home/views/widgets/for_you.dart';
 import 'package:whatsevr_app/src/features/home/views/widgets/offers.dart';
 part 'widgets/history.dart';
+part 'widgets/bookmarks.dart';
 class HomePage extends StatelessWidget {
   HomePage({super.key});
   final ScrollController searchBoxHideController = ScrollController();
@@ -49,7 +52,7 @@ class HomePage extends StatelessWidget {
                 ('Activities', WhatsevrTabBarWithViews(
       tabViews: [
         ('History', _HistoryView()),
-        ('Saved', Text('Saved')),
+        ('Saved', _BookmarksView()),
       ], 
     )),
               ],
@@ -60,173 +63,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
-// class HomePageActivitiesPage extends StatelessWidget {
-//   const HomePageActivitiesPage({
-//     super.key,
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return const WhatsevrTabBarWithViews(
-//       tabs: <String>[
-//         'History',
-//         'Offers',
-//         'Saved',
-//         'Playlists',
-//       ],
-//       tabViews: <Widget>[
-//         Text('History'),
-//         Text('Offers'),
-//         Text('Playlists'),
-//         Text('Saved'),
-//       ],
-//     );
-//   }
-// }
-//
-// class HomePageCommunitiesPage extends StatelessWidget {
-//   const HomePageCommunitiesPage({
-//     super.key,
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Text('Media');
-//   }
-// }
-//
-// class HomePageForYouPage extends StatelessWidget {
-//   const HomePageForYouPage({
-//     super.key,
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: <Widget>[
-//         SizedBox(
-//           height: 120.0,
-//           child: Builder(
-//             builder: (BuildContext context) {
-//               List<Widget> children = <Widget>[
-//                 Column(
-//                   children: <Widget>[
-//                     Expanded(
-//                       child: Container(
-//                         decoration: BoxDecoration(
-//                           color: Colors.blue,
-//                           borderRadius: BorderRadius.circular(18.0),
-//                         ),
-//                         width: 100.0,
-//                         alignment: Alignment.center,
-//                         child: Container(
-//                           padding: const EdgeInsets.all(10.0),
-//                           decoration: const BoxDecoration(
-//                             color: Colors.white,
-//                             shape: BoxShape.circle,
-//                           ),
-//                           child: const Icon(
-//                             Icons.add,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     const Gap(24.0),
-//                   ],
-//                 ),
-//                 for (int i = 0; i < 5; i++)
-//                   Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: <Widget>[
-//                       Expanded(
-//                         child: Container(
-//                           decoration: BoxDecoration(
-//                             color:
-//                                 Colors.primaries[i % Colors.primaries.length],
-//                             borderRadius: BorderRadius.circular(18.0),
-//                             image: DecorationImage(
-//                               image: ExtendedNetworkImageProvider(
-//                                 MockData.randomImage(),
-//                               ),
-//                               fit: BoxFit.cover,
-//                             ),
-//                           ),
-//                           width: 180.0,
-//                           alignment: Alignment.center,
-//                         ),
-//                       ),
-//                       const Gap(5.0),
-//                       Row(
-//                         children: <Widget>[
-//                           CircleAvatar(
-//                             backgroundImage: ExtendedNetworkImageProvider(
-//                               MockData.imageAvatar,
-//                             ),
-//                             radius: 10.0,
-//                           ),
-//                           const Gap(5.0),
-//                           const Text('Username'),
-//                         ],
-//                       ),
-//                     ],
-//                   ),
-//               ];
-//               return ListView.separated(
-//                 scrollDirection: Axis.horizontal,
-//                 itemBuilder: (BuildContext context, int index) {
-//                   return Padding(
-//                     padding: EdgeInsets.only(
-//                       left: index == 0 ? PadHorizontal.paddingValue : 0.0,
-//                       right: index == children.length - 1
-//                           ? PadHorizontal.paddingValue
-//                           : 0.0,
-//                     ),
-//                     child: children[index],
-//                   );
-//                 },
-//                 separatorBuilder: (BuildContext context, int index) {
-//                   return const Gap(5.0);
-//                 },
-//                 itemCount: children.length,
-//               );
-//             },
-//           ),
-//         ),
-//         const Gap(8.0),
-//         Expanded(
-//           child: GridView.custom(
-//             gridDelegate: SliverQuiltedGridDelegate(
-//               crossAxisCount: 3,
-//               mainAxisSpacing: 2,
-//               crossAxisSpacing: 2,
-//               repeatPattern: QuiltedGridRepeatPattern.inverted,
-//               pattern: <QuiltedGridTile>[
-//                 QuiltedGridTile(
-//                   2,
-//                   1,
-//                 ),
-//                 QuiltedGridTile(1, 1),
-//                 QuiltedGridTile(1, 1),
-//                 QuiltedGridTile(1, 1),
-//                 QuiltedGridTile(1, 1),
-//                 QuiltedGridTile(2, 3),
-//               ],
-//             ),
-//             childrenDelegate: SliverChildBuilderDelegate(
-//               (BuildContext context, int index) {
-//                 if (index % 6 == 0) return const FlickPostTile();
-//
-//                 if (index % 6 == 1) return const PhotoPostTile();
-//                 if (index % 6 == 2) return const PhotoPostTile();
-//                 if (index % 6 == 3) return const PhotoPostTile();
-//                 if (index % 6 == 4) return const VideoPostTile();
-//                 return const PortfolioVideoPostTile();
-//               },
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
---
+++ lib/src/features/home/views/widgets/bookmarks.dart
@@ -0,0 +1,376 @@
+part of '../page.dart';
+
+class BookmarkFolder {
+  final String id;
+  final String name;
+  final String? icon;
+  final DateTime createdAt;
+  final List<SavedContent> items;
+
+  BookmarkFolder({
+    required this.id,
+    required this.name,
+    this.icon,
+    required this.createdAt,
+    required this.items,
+  });
+}
+
+class SavedContent {
+  final String uid;
+  final ContentType type;
+  final String title;
+  final String? thumbnail;
+  final DateTime savedAt;
+  final String? description;
+  final Map<String, dynamic>? metadata;
+
+  SavedContent({
+    required this.uid,
+    required this.type,
+    required this.title,
+    this.thumbnail,
+    required this.savedAt,
+    this.description,
+    this.metadata,
+  });
+}
+
+class _BookmarksView extends StatefulWidget {
+  const _BookmarksView({super.key});
+
+  @override
+  State<_BookmarksView> createState() => _BookmarksViewState();
+}
+
+class _BookmarksViewState extends State<_BookmarksView> {
+  BookmarkFolder? _selectedFolder;
+  
+  final List<BookmarkFolder> _folders = [
+    BookmarkFolder(
+      id: '1',
+      name: 'Tutorials',
+      icon: '📚',
+      createdAt: DateTime.now(),
+      items: [
+        SavedContent(
+          uid: '1',
+          type: ContentType.wtv,
+          title: 'Flutter Animation Tutorial',
+          thumbnail: 'https://picsum.photos/200/200?random=1',
+          savedAt: DateTime.now(),
+          description: 'Learn advanced animations in Flutter',
+          metadata: {'duration': '10:30', 'views': 1200},
+        ),
+        SavedContent(
+          uid: '2',
+          type: ContentType.pdf,
+          title: 'UI Design Guidelines',
+          thumbnail: 'https://picsum.photos/200/200?random=2',
+          savedAt: DateTime.now(),
+          metadata: {'pages': 25},
+        ),
+      ],
+    ),
+    BookmarkFolder(
+      id: '2',
+      name: 'Inspiration',
+      icon: '💡',
+      createdAt: DateTime.now(),
+      items: [
+        SavedContent(
+          uid: '3',
+          type: ContentType.photo,
+          title: 'Modern UI Examples',
+          thumbnail: 'https://picsum.photos/200/200?random=3',
+          savedAt: DateTime.now(),
+        ),
+        SavedContent(
+          uid: '4',
+          type: ContentType.flick,
+          title: 'Design Process',
+          thumbnail: 'https://picsum.photos/200/200?random=4',
+          savedAt: DateTime.now(),
+          metadata: {'duration': '3:45'},
+        ),
+      ],
+    ),
+    // Add more sample folders...
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return _selectedFolder == null
+        ? _buildFoldersGrid()
+        : _buildFolderContent();
+  }
+
+  Widget _buildFoldersGrid() {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(context.whatsevrTheme.spacing2),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'My Collections',
+                style: context.whatsevrTheme.h3,
+              ),
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  // Show create folder dialog
+                },
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: GridView.builder(
+            padding: EdgeInsets.all(context.whatsevrTheme.spacing2),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: context.whatsevrTheme.spacing2,
+              crossAxisSpacing: context.whatsevrTheme.spacing2,
+              childAspectRatio: 1,
+            ),
+            itemCount: _folders.length,
+            itemBuilder: (context, index) {
+              final folder = _folders[index];
+              return _FolderCard(
+                folder: folder,
+                onTap: () => setState(() => _selectedFolder = folder),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildFolderContent() {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(context.whatsevrTheme.spacing2),
+          decoration: BoxDecoration(
+            color: context.whatsevrTheme.surface,
+            boxShadow: [context.whatsevrTheme.boxShadow],
+          ),
+          child: Row(
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () => setState(() => _selectedFolder = null),
+              ),
+              const Gap(8),
+              Text(
+                _selectedFolder!.name,
+                style: context.whatsevrTheme.subtitle,
+              ),
+              const Spacer(),
+              Text(
+                '${_selectedFolder!.items.length} items',
+                style: context.whatsevrTheme.caption,
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _selectedFolder!.items.length,
+            itemBuilder: (context, index) {
+              final item = _selectedFolder!.items[index];
+              return _SavedItemTile(item: item);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _FolderCard extends StatelessWidget {
+  final BookmarkFolder folder;
+  final VoidCallback onTap;
+
+  const _FolderCard({
+    required this.folder,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: context.whatsevrTheme.borderRadiusSmall,
+      child: Container(
+        padding: EdgeInsets.all(context.whatsevrTheme.spacing2),
+        decoration: BoxDecoration(
+          color: context.whatsevrTheme.surface,
+          borderRadius: context.whatsevrTheme.borderRadiusSmall,
+          boxShadow: [context.whatsevrTheme.boxShadow],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              folder.icon ?? '📁',
+              style: TextStyle(fontSize: 32),
+            ),
+            const Gap(8),
+            Text(
+              folder.name,
+              style: context.whatsevrTheme.subtitle,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            const Gap(4),
+            Text(
+              '${folder.items.length} items',
+              style: context.whatsevrTheme.caption,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SavedItemTile extends StatelessWidget {
+  final SavedContent item;
+
+  const _SavedItemTile({required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: context.whatsevrTheme.spacing2,
+        vertical: context.whatsevrTheme.spacing1,
+      ),
+      decoration: BoxDecoration(
+        color: context.whatsevrTheme.surface,
+        borderRadius: context.whatsevrTheme.borderRadiusSmall,
+        boxShadow: [context.whatsevrTheme.boxShadow],
+      ),
+      child: InkWell(
+        onTap: () {
+          // Handle item tap
+        },
+        child: Padding(
+          padding: EdgeInsets.all(context.whatsevrTheme.spacing2),
+          child: Row(
+            children: [
+              if (item.thumbnail != null)
+                ClipRRect(
+                  borderRadius: context.whatsevrTheme.borderRadiusSmall,
+                  child: Image.network(
+                    item.thumbnail!,
+                    width: 80,
+                    height: 80,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              const Gap(12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          ActivityItem.getContentTypeIcon(item.type),
+                          size: 16,
+                          color: context.whatsevrTheme.icon,
+                        ),
+                        const Gap(4),
+                        Text(
+                          item.type.name.toUpperCase(),
+                          style: context.whatsevrTheme.caption,
+                        ),
+                      ],
+                    ),
+                    const Gap(4),
+                    Text(
+                      item.title,
+                      style: context.whatsevrTheme.subtitle.copyWith(
+                        fontSize: 14,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    if (item.description != null) ...[
+                      const Gap(4),
+                      Text(
+                        item.description!,
+                        style: context.whatsevrTheme.caption,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                    const Gap(4),
+                    _buildMetadataRow(context),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMetadataRow(BuildContext context) {
+    final List<Widget> metadata = [];
+
+    if (item.metadata != null) {
+      if (item.metadata!['duration'] != null) {
+        metadata.add(Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.timer_outlined, size: 12, color: context.whatsevrTheme.textLight),
+            const Gap(4),
+            Text(
+              item.metadata!['duration'],
+              style: context.whatsevrTheme.caption,
+            ),
+          ],
+        ));
+      }
+
+      if (item.metadata!['views'] != null) {
+        metadata.add(Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.visibility_outlined, size: 12, color: context.whatsevrTheme.textLight),
+            const Gap(4),
+            Text(
+              NumberFormat.compact().format(item.metadata!['views']),
+              style: context.whatsevrTheme.caption,
+            ),
+          ],
+        ));
+      }
+    }
+
+    return Row(
+      children: [
+        if (metadata.isNotEmpty) ...[
+          ...metadata.asMap().entries.map((entry) => [
+            entry.value,
+            if (entry.key < metadata.length - 1) const Gap(8),
+          ]).expand((x) => x),
+          const Spacer(),
+        ],
+        Text(
+          timeago.format(item.savedAt),
+          style: context.whatsevrTheme.caption,
+        ),
+      ],
+    );
+  }
+}
+
+
